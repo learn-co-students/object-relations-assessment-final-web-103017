df8143e53785738891f79006cff463c6c3f07af2
dansbands <dansbands@hotmail.com>
2017-11-17 10:50:21 -0500

    mostly done

--- app/models/watchlisting.rb
+++ app/models/watchlisting.rb
@@ -1,4 +1,17 @@
 class WatchListing
+  attr_accessor :viewer, :movie, :rating
 
+  @@all = []
 
+  def initialize(viewer, movie, rating=5)
+    @viewer = viewer
+    @movie = movie
+    @rating = rating
+    @@all << self
+  end
+
+  def self.all
+    # returns all of watchlistings
+    @@all
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,21 @@ def reload
   load 'config/environment.rb'
 end
 
+dan = Viewer.new("Dan")
+melissa = Viewer.new("Melissa")
+
+star_wars = Movie.new("Star Wars")
+lebowski = Movie.new("The Big Lebowski")
+clerks = Movie.new("Clerks")
+
+movie_array = [clerks, lebowski]
+movie_array2 = [star_wars, clerks]
+
+new_wl = dan.add_to_watchlist(movie_array)
+new_wl2 = melissa.add_to_watchlist(movie_array)
+
+
+
+
+
 Pry.start
